1bebc922f1cebd8db79da70cf55954991e083676
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2026-04-21 21:52:18 -0300

    fix: try deploy with --force flag

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,15 +3,12 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ligadelincoln.com",
-  output: "static",
-  adapter: vercel(),
   integrations: [react(), sitemap()],
 
   vite: {
